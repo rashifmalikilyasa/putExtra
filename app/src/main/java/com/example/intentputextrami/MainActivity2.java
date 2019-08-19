8ebf113efba87911e01c04002227e2022199e3c3
zeptian15 <zeptian15@gmail.com>
2019-08-19 14:28:31 +0700

    first commit

---
+++ app/src/main/java/com/example/intentputextrami/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.intentputextrami;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = (TextView)findViewById(R.id.text_viewdata);
+        Bundle bundle = getIntent().getExtras();
+        String s = bundle.getString("name");
+        t.setText(s);
+    }
+}
